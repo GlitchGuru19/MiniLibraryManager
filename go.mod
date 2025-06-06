0dd62475ac6af438dd114b1be17025373271dbc3
GlitchGuru19 <peterkatebe12@gmail.com>
2025-06-06 21:40:45 +0200

    Creating a mini library manager

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GlitchGuru19/MiniLibraryManager
+
+go 1.23.6
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("\nWelcome to the Mini Library Manager")
+	fmt.Printf("please choose an option")
+	fmt.Println("1. Add Book")
+	fmt.Println("2. List Books")
+	fmt.Println("3. Borrow Book")
+	fmt.Println("4. Return Book")
+	fmt.Println("5. Exit")
+}
