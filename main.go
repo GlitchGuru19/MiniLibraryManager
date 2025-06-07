1418d2a133471f370466e04e372c9a1e1242d361
GlitchGuru19 <peterkatebe12@gmail.com>
2025-06-07 14:13:56 +0200

    getting user choice using an int variable

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
+// Mini Library Manager
+/* User can add, list, borrow and return books and they can exit 
+*/
+
 package main
 
 import "fmt"
 
-func main(){
+func main() {
+	var option int
+
 	fmt.Println("\nWelcome to the Mini Library Manager")
-	fmt.Printf("please choose an option")
+	fmt.Println()
+	fmt.Println("please choose an option")
 	fmt.Println("1. Add Book")
 	fmt.Println("2. List Books")
 	fmt.Println("3. Borrow Book")
 	fmt.Println("4. Return Book")
 	fmt.Println("5. Exit")
+	fmt.Print("Choice => ")
+	fmt.Scanln(&option)
+
+	fmt.Println("Option: ", option, "has been chosen")
+
+	fmt.Println()
 }
