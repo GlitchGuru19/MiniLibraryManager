c6014e79c57f8715ab3bf0a59e76d6af8e419234
GlitchGuru19 <peterkatebe12@gmail.com>
2025-06-09 22:28:54 +0200

    Created the separate functions necessary

--- main.go
+++ main.go
@@ -1,52 +1,116 @@
 // Mini Library Manager
-/* User can add, list, borrow and return books and they can exit 
-*/
+/* User can add, list, borrow and return books and they can exit
+ */
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// A simple struct with exported fields
 type Book struct{
 	Title string 
 	Author string 
 	Year int
+	// isBorrowed bool, comented out to test and run the rest..
 }
 
-
-func listBooks(){
-	fmt.Println("Function to display all the books")
-}
-
-
-func addBook(){
-	fmt.Println("Function to add books")
-}
-
-func main() {
-	var option int
-
-	books := Book{
-		Title: "Harry Potter",
-		Author: "JK Rolins",
-		Year: 2002,
-	}
-
-	fmt.Println(books.Title)
-	fmt.Println(books.Author)
-	fmt.Println(books.Year)
-
+// Function to display the menu
+func displayMenu(){
 	fmt.Println("\nWelcome to the Mini Library Manager")
 	fmt.Println()
-	fmt.Println("please choose an option")
+	fmt.Println("please select an option")
 	fmt.Println("1. Add Book")
 	fmt.Println("2. List Books")
 	fmt.Println("3. Borrow Book")
 	fmt.Println("4. Return Book")
 	fmt.Println("5. Exit")
-	fmt.Print("Choice => ")
-	fmt.Scanln(&option)
+}
 
-	fmt.Println("Option: ", option, "has been chosen")
+// Function to add a book
+func addBook(){
+	// Slice of type Book
+	var book []Book
+	// Here we variables we will pass into the slice 
+	var (
+		title string
+		author string
+		year int
+		// isBorrowed bool
+	)
 
-	fmt.Println()
+	fmt.Println("Add a book")
+	fmt.Print("Enter the Title: ")
+	fmt.Scanln(&title)
+	fmt.Print("Enter the Author: ")
+	fmt.Scanln(&author)
+	fmt.Print("Enter the Year: ")
+	fmt.Scanln(&year)
+
+	books := Book{Title: title, Author: author, Year: year}
+	book = append(book, books)
+	fmt.Println("Book added successfully")
+}
+
+// Function to add a book
+func borrowBook(){
+	// function to borrow a book
+}
+
+// Function to Return a borrowed book
+func returnBook(){
+	// Function to return a book
+}
+
+// Function to display/list all the books
+func listBooks(book []Book){
+	fmt.Println("Function to display all the books")
+
+}
+
+
+func main() {
+	var option int
+
+	/* this is just for testing purposes, as we want to know if it is working or not
+	books := Book{
+		Title: "Harry Potter",
+		Author: "JK Rolins",
+		Year: 2002,
+		isBorrowed: true,
+	} */
+
+	// fmt.Println(books.Title)
+	// fmt.Println(books.Author)
+	// fmt.Println(books.Year)
+	// fmt.Println(books.isBorrowed)
+
+
+	for {
+
+		displayMenu()
+		fmt.Print("Option => ")
+		fmt.Scanln(&option)
+
+		switch option {
+		case 1:
+			addBook()
+		case 2:
+			//listBooks()
+		case 3:
+			//borrowBook()
+			fmt.Println("Borrow book")
+		case 4:
+			//returnBook()
+			fmt.Println("Return book?")
+		case 5:
+			fmt.Println("Thank you for using the program.")
+			fmt.Println()
+			return
+		default:
+			fmt.Println("Wrong option. Please ty again.")
+
+		}
+	}
 }
